348e2f18c1a74c3a621fe2ade6937aa73c7711c2
acesavvy101 <gracielladeronne07@gmail.com>
2026-05-12 01:32:43 +0800

    made a new grid to replace the old one after user input, in the same total space as before

--- script.js
+++ script.js
@@ -21,11 +21,28 @@ button.style.cssText ="color:#f06e95; background-color:#276ce4"
 
 function popup () {
     let input = prompt ("Enter a positive number less than 100 for the new grid:" , " ")
-    if (input > 100 || input < 1) {
+    if (input > 100 || input < 1 || input == "string") {
         alert ("pick a positive number");
     } else {
     // replace the grid in the container
+    const newGridSize = input * input;
     container.replaceChildren();
+    const newSize = 800/input;
+
+    for (let i = 1; i <= newGridSize ; i++) {
+    const newGrid = document.createElement("div");
+    newGrid.style.cssText = `width: ${newSize}px; height:${newSize}px; border:2px solid black`;
+    /* line 34 Previous Syntax Error : i had a feeling u needed to use `` and ${} for the variable 
+    but i wasnt sure if it was going to work or not */
+    container.appendChild(newGrid);
+
+    function newChangeColor () {
+        newGrid.style.cssText = `width: ${newSize}px; height:${newSize}px; border:2px solid black ; background-color: black`;
+    }
+    newGrid.addEventListener('mouseenter' , newChangeColor);    
+    }
     }
 }
 button.addEventListener ("click" , popup);
+
+//square size must shrink as grid increases, control squares per side not total squares created
